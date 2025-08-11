c48bd4a9e236259d665d23c4ec2dcb5c22aafa38
youssef <yussefhassan600@gmail.com>
2025-08-12 02:08:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+var titleArr=[];
+var contentArr=[];
+var length=0;
+
+const app=express();
+const port=3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+res.render("index.ejs",{title:titleArr,content:contentArr})
+});
+app.get("/home",(req,res)=>{
+res.render("index.ejs",{title:titleArr,content:contentArr})
+
+});
+app.get("/about",(req,res)=>{
+res.render("about.ejs");
+});
+app.get("/contact",(req,res)=>{
+res.render("contact.ejs");
+});
+
+
+
+app.post("/creation",(req,res)=>{
+res.render("createPost.ejs");
+});
+
+app.post("/home",(req,res)=>{
+
+titleArr[length]=req.body.title;
+contentArr[length]=req.body.content;
+
+
+res.render("index.ejs",{title:titleArr,content:contentArr});
+
+length++;
+});
+
+app.post("/view",(req,res)=>{
+var pos=parseInt(req.body.position, 10);;
+res.render("viewPost.ejs",{title:titleArr[pos],content:contentArr[pos]});
+});
+
+app.post("/edit",(req,res)=>{
+var pos=parseInt(req.body.position, 10);;
+res.render("editPost.ejs",{title:titleArr[pos],content:contentArr[pos],i:pos});
+});
+
+app.post("/editWords",(req,res)=>{
+var pos=parseInt(req.body.position, 10);;
+titleArr[pos]=req.body.title;
+contentArr[pos]=req.body.content;
+
+res.render("index.ejs",{title:titleArr,content:contentArr});
+});
+
+app.post("/delete",(req,res)=>{
+var pos=parseInt(req.body.position, 10);;
+titleArr.splice(pos,1);
+contentArr.splice(pos,1);
+
+res.render("index.ejs",{title:titleArr,content:contentArr});
+length--;
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running now on port ${port}`);
+});
